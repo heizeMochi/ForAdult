46a656441902b95c287036d32f90f01771fee60d
HeizeMochi <heizeMochi@gmail.com>
2020-12-03 16:08:12 +0900

    EVENT SYSTEM

---
+++ Assets/Scripts/UIClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIClick : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    Image img;
+    [SerializeField]
+    Sprite enterImg;
+
+    Sprite defaultImg;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+        defaultImg = img.sprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name);
+        img.sprite = enterImg;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        img.sprite = defaultImg;
+    }
+}
