144ecc2439a72de5745496b73b61d0a28ada0d20
HeizeMochi <heizeMochi@gmail.com>
2020-12-13 14:07:09 +0900

    Present Click Text

--- Assets/Scripts/PresentClick.cs
+++ Assets/Scripts/PresentClick.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 
 public class PresentClick : MonoBehaviour
 {
+    public string chat;
     public GameObject GoTakeOut;
 
     private void Update()
+    {
+        InputFunc();
+    }
+
+    void InputFunc()
     {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.forward, 10f);
 
         if (Input.GetMouseButtonDown(0))
         {
+            if (hit == false)
+                return;
             if (hit.collider.gameObject == gameObject)
             {
                 Managers.Game.FindPresent((Define.Present)Enum.Parse(typeof(Define.Present), gameObject.name), gameObject.name);
+                Managers.Talk.Talk(gameObject.name, chat);
                 GoTakeOut.SetActive(true);
             }
         }
