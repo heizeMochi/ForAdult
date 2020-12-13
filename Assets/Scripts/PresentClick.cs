8aa88c2950e33e1fe3864de12750e4fd58929df1
HeizeMochi <heizeMochi@gmail.com>
2020-12-13 13:44:56 +0900

    PresentClick

---
+++ Assets/Scripts/PresentClick.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentClick : MonoBehaviour
+{
+    public GameObject GoTakeOut;
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.forward, 10f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider.gameObject == gameObject)
+            {
+                Managers.Game.FindPresent((Define.Present)Enum.Parse(typeof(Define.Present), gameObject.name), gameObject.name);
+                GoTakeOut.SetActive(true);
+            }
+        }
+    }
+}
